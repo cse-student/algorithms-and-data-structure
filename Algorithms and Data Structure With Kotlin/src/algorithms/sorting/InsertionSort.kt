6d75ecc78b03d86556e60703cf1db47a70d3df21
akhilesh <akhileshseechurn@gmail.com>
2019-05-19 21:59:50 +0400

    [algorithms-and-data-structure][Algorithms and Data Structure With Kotlin] Insertion sort bug fix

--- Algorithms and Data Structure With Kotlin/src/algorithms/sorting/InsertionSort.kt
+++ Algorithms and Data Structure With Kotlin/src/algorithms/sorting/InsertionSort.kt
@@ -6,24 +6,15 @@ class InsertionSort {
     companion object : ISort {
         override fun sort(list: MutableList<Int>): MutableList<Int> {
             var result = list
-            for (i in 0 until result.count()) {
+            for (i in 1 until result.count()) {
                 var current = result[i]
-                var newPos = i
-                while (result[newPos] > current) {
+                var newPos = i-1
+                while (newPos >= 0 && result[newPos] > current) {
+                    result[newPos+1] = result[newPos]
                     newPos--
                 }
-                result = swap(result, i, newPos)
+                result[newPos+1] = current
             }
-            return result
-        }
-
-        fun swap(list: MutableList<Int>, oldPos: Int, newPos: Int): MutableList<Int> {
-            var result = list
-            var value = list[oldPos]
-            for(i in oldPos until newPos){
-                result[i] = result[i-1]
-            }
-            result[newPos] = value
             return result
         }
     }
